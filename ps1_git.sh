5947337ea9a8b81908da534206b93c6145237c98
somla <edesalmos@hotmail.com>
2020-02-03 15:42:03 +0100

    [m] ps1_git.sh some review fix

--- ps1_git.sh
+++ ps1_git.sh
@@ -1,5 +1,5 @@
 function ps1_git_branch {
-    local branch=`git branch 2>/dev/null | grep '^*' | colrm 1 2`;
+    local branch="`git branch 2>/dev/null | grep '^*' | colrm 1 2`";
     if [ "$branch" != "" ]; then
         local branch=":$branch";
     fi
@@ -7,11 +7,11 @@ function ps1_git_branch {
 }
 
 function ps1_git_postfix {
-    local project_root=`git rev-parse --show-toplevel 2>/dev/null`
+    local project_root="`git rev-parse --show-toplevel 2>/dev/null`"
     if [ "$project_root" != "" ]; then
-        local project_root="`dirname $project_root`";
-        local postfix="`pwd | sed "s@^$project_root/@@g"`";
-        echo $postfix;
+        local project_root="`dirname -- "$project_root"`";
+        local postfix="`pwd | sed --sandbox "s@^$project_root/@@g"`";
+        echo "$postfix";
     fi
 }
 
@@ -19,11 +19,11 @@ function ps1_git_prefix {
     local project_root="`git rev-parse --show-toplevel 2>/dev/null`";
     local dir=""
     if [ "$project_root" != "" ]; then
-        local dir=`dirname $project_root`/;
+        local dir="`dirname -- $project_root`/";
     else
-        local dir=`pwd`
+        local dir="`pwd`"
     fi
-    echo $dir | sed -e "s@$HOME@~@g"
+    echo $dir | sed --sandbox "s@$HOME@~@g"
 }
 
 if [ "$color_prompt" = yes ]; then
